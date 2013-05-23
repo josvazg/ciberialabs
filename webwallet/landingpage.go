6169bb2460f5dd9d6f82a8b9b0be73d0cce2985a
nunes <nunes@es-robot.com>
2013-05-23 17:26:53 +0200

    google tutorial

--- webwallet/landingpage.go
+++ webwallet/landingpage.go
@@ -1,26 +1,72 @@
 package webwallet
 
 import (
-	//"appengine"
-	//"appengine/datastore"
-	//"appengine/user"
-	"fmt"
-	//"html/template"
-	"net/http"
-	//"time"
+    "appengine"
+    "appengine/datastore"
+    "appengine/user"
+    "html/template"
+    "net/http"
+    "time"
 )
 
-/*var templates = template.Must(template.ParseFiles("html/browse.html", "html/add.html",
-    "html/templates.html","html/style.css"))*/
+type Greeting struct {
+    Author  string
+    Content string
+    Date    time.Time
+}
 
 func init() {
-/*    http.HandleFunc("/login", login)
-    http.HandleFunc("/add",add)*/
-    http.HandleFunc("/", landingpage)
+    http.HandleFunc("/", root)
+    http.HandleFunc("/sign", sign)
 }
 
-func landingpage(w http.ResponseWriter, r *http.Request) {
-	//c := appengine.NewContext(r)
-	 fmt.Fprint(w, "Hello, world!")
+func root(w http.ResponseWriter, r *http.Request) {
+    c := appengine.NewContext(r)
+    q := datastore.NewQuery("Greeting").Order("-Date").Limit(10)
+    greetings := make([]Greeting, 0, 10)
+    if _, err := q.GetAll(c, &greetings); err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    if err := guestbookTemplate.Execute(w, greetings); err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
 }
 
+var guestbookTemplate = template.Must(template.New("book").Parse(guestbookTemplateHTML))
+
+const guestbookTemplateHTML = `
+<html>
+  <body>
+    {{range .}}
+      {{with .Author}}
+        <p><b>{{.}}</b> wrote:</p>
+      {{else}}
+        <p>An anonymous person wrote:</p>
+      {{end}}
+      <pre>{{.Content}}</pre>
+    {{end}}
+    <form action="/sign" method="post">
+      <div><textarea name="content" rows="3" cols="60"></textarea></div>
+      <div><input type="submit" value="Sign Guestbook"></div>
+    </form>
+  </body>
+</html>
+`
+
+func sign(w http.ResponseWriter, r *http.Request) {
+    c := appengine.NewContext(r)
+    g := Greeting{
+        Content: r.FormValue("content"),
+        Date:    time.Now(),
+    }
+    if u := user.Current(c); u != nil {
+        g.Author = u.String()
+    }
+    _, err := datastore.Put(c, datastore.NewIncompleteKey(c, "Greeting", nil), &g)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    http.Redirect(w, r, "/", http.StatusFound)
+}
