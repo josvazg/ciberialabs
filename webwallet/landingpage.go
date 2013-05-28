6fc73334dc553f5c9f40c2d76f99cf9358fc7fd3
nunes <nunes@es-robot.com>
2013-05-28 15:55:47 +0200

    added template folder

--- webwallet/landingpage.go
+++ webwallet/landingpage.go
@@ -28,31 +28,16 @@ func root(w http.ResponseWriter, r *http.Request) {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
+    
+	var guestbookTemplate = template.Must(template.ParseFiles("./webwallet/templates/guestbook.html"))
+
     if err := guestbookTemplate.Execute(w, greetings); err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
     }
 }
 
-var guestbookTemplate = template.Must(template.New("book").Parse(guestbookTemplateHTML))
 
-const guestbookTemplateHTML = `
-<html>
-  <body>
-    {{range .}}
-      {{with .Author}}
-        <p><b>{{.}}</b> wrote:</p>
-      {{else}}
-        <p>An anonymous person wrote:</p>
-      {{end}}
-      <pre>{{.Content}}</pre>
-    {{end}}
-    <form action="/sign" method="post">
-      <div><textarea name="content" rows="3" cols="60"></textarea></div>
-      <div><input type="submit" value="Sign Guestbook"></div>
-    </form>
-  </body>
-</html>
-`
+
 
 func sign(w http.ResponseWriter, r *http.Request) {
     c := appengine.NewContext(r)
